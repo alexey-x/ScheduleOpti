737d89f8925ae1f058471bed9a79bd912aa06da4
alexey-x <avoevodkin@gmail.com>
2021-08-12 22:33:39 +0300

    Add order-matrix name

--- schedule/src/order.py
+++ schedule/src/order.py
@@ -1,8 +1,9 @@
 
 class Order:
-    def __init__(self, order_index: int, order_duration: int):
+    def __init__(self, order_index: int, order_duration: int, order_name: str):
         self.order_index = order_index
         self.order_duration = order_duration
+        self.order_name = order_name
         self.heated = False
 
     def get_order_index(self):
@@ -19,3 +20,6 @@ class Order:
 
     def __repr__(self):
         return f"Order({self.order_index}: {self.order_duration}, heated={self.heated})"
+
+    def __str__(self):
+        return f"{self.get_order_index}, {self.order_name}: {self.order_duration}"
