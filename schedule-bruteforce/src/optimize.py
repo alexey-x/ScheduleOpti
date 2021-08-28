b7e480f5253be4fc89ae528c95d8d168dd4e6f00
alexey-x <avoevodkin@gmail.com>
2021-08-28 22:41:35 +0300

    Add cycle number

--- schedule-bruteforce/src/optimize.py
+++ schedule-bruteforce/src/optimize.py
@@ -11,17 +11,18 @@ from src.sub_tasks import get_permutations
 
 def get_run_time(
     orders: List[Order], strategy: WorkingStrategy
-) -> Tuple[int, List[Order]]:
+) -> Tuple[int, int, List[Order]]:
     press = Press(strategy)
     press.run(deepcopy(orders))
-    return press.total_work_time, orders
+    return press.total_work_time, press.cycle_number, orders
 
 
 def brute_force_optimize(
     sequence: List[Order], strategy: WorkingStrategy, batchsize: int
-) -> Tuple[int, List[List[Order]]]:
+) -> Tuple[int, int, List[List[Order]]]:
     all_perm = get_permutations(sequence, NSLOTS)
     best_time = sum((order.get_order_duration() for order in sequence))
+    best_cycle_number = 0
     best_sequence = []
     while True:
         pool = Pool()
@@ -30,13 +31,14 @@ def brute_force_optimize(
             for perm in list(islice(all_perm, batchsize))
         ]
         print(f"Batch calc is done. Pool length = {len(_result)}")
-        for runtime, runseq in _result:
+        for runtime, cycle_number, runseq in _result:
             if best_time == runtime:
                 best_sequence.append(runseq)
             elif best_time > runtime:
                 best_time = runtime
                 best_sequence = [runseq]
+            best_cycle_number = cycle_number
         print("Local best sequence is found.")
         if len(_result) == 0:
             break
-    return best_time, best_sequence
+    return best_time, best_cycle_number, best_sequence
