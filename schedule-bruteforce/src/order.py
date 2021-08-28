616782cf05c308e378fc668a398838045fde184a
alexey-x <avoevodkin@gmail.com>
2021-08-28 19:10:01 +0300

    Add set sate

--- schedule-bruteforce/src/order.py
+++ schedule-bruteforce/src/order.py
@@ -7,7 +7,7 @@ class Order:
         self._order_name = order_name
         self._order_length = order_duration
         self._heated = False
-        self._order_state = "change"
+        self.__set_order_state()
 
     def get_order_index(self) -> int:
         return self._order_index
@@ -24,13 +24,24 @@ class Order:
     def get_order_state(self) -> str:
         return self._order_state
 
+    def __set_order_state(self) -> None:
+        if not self._heated:
+            self._order_state = "new"
+        elif self._heated and self._order_duration == self._order_length:
+            self._order_state = "heat"
+        elif self._heated and 0 < self._order_duration < self._order_length:
+            self._order_state = "work"
+        elif self._heated and self._order_duration == 0:
+            self._order_state = "done"
+        else:
+            self._order_state = "UNEXPECTED"
+
     def process_order(self, order_process_time: int) -> None:
         if self._heated:
             self._order_duration = max(0, self._order_duration - order_process_time)
-            self._order_state = "work"
         else:
             self._heated = True
-            self._order_state = "heat"
+        self.__set_order_state()
 
     def __repr__(self):
         return f"Order({self._order_index}: {self._order_duration}, heated={self._heated})"
