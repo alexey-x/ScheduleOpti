c0e7ade34af0ab4cb3910c53c15c82b6b8dc19bd
alexey-x <avoevodkin@gmail.com>
2021-08-28 18:15:26 +0300

    Add order state

--- schedule-bruteforce/src/order.py
+++ schedule-bruteforce/src/order.py
@@ -1,4 +1,5 @@
 
+
 class Order:
     def __init__(self, order_index: int, order_duration: int, order_name: str):
         self._order_index = order_index
@@ -6,6 +7,7 @@ class Order:
         self._order_name = order_name
         self._order_length = order_duration
         self._heated = False
+        self._order_state = "change"
 
     def get_order_index(self) -> int:
         return self._order_index
@@ -19,11 +21,16 @@ class Order:
     def get_order_name(self) -> str:
         return self._order_name
 
+    def get_order_state(self) -> str:
+        return self._order_state
+
     def process_order(self, order_process_time: int) -> None:
         if self._heated:
             self._order_duration = max(0, self._order_duration - order_process_time)
+            self._order_state = "work"
         else:
             self._heated = True
+            self._order_state = "heat"
 
     def __repr__(self):
         return f"Order({self._order_index}: {self._order_duration}, heated={self._heated})"
