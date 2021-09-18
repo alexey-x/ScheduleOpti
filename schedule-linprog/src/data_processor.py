6b5591de938667822fd675b00de1bc3467d7b8e5
alexey-x <avoevodkin@gmail.com>
2021-09-18 18:39:48 +0300

    Add min_time_interval getter

--- schedule-linprog/src/data_processor.py
+++ schedule-linprog/src/data_processor.py
@@ -26,6 +26,9 @@ class DataProcessor:
     def get_heating_time(self) -> int:
         return self.const.THEAT
 
+    def get_min_number_interval(self) -> int:
+        return (self.get_orders_number() + 2)//3
+
     def get_time_steps(self) -> List[int]:
         if self.num_time_interval is None:
             time_steps_number = max(2, len(self.orders) - 1)
@@ -36,5 +39,6 @@ class DataProcessor:
     def get_min_working_time(self) -> int:
         """Most general estimation - one order may be so long that others finish earlier."""
         init_time = self.const.TCHANGE + self.const.THEAT
-        max_duration = max([i for i in self.get_durations().values()])
+        #max_duration = max([i for i in self.get_durations().values()])
+        max_duration = max(self.get_durations().values())
         return init_time + max_duration
