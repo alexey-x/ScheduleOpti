958ca79f8c6dc33eaff06c4fc3122d9031731a52
alexey-x <avoevodkin@gmail.com>
2022-02-24 14:44:18 +0300

    Move read write to separate file

--- schedule-bruteforce/SchedulePrintDoor.py
+++ schedule-bruteforce/SchedulePrintDoor.py
@@ -1,47 +1,22 @@
 import click
 import datetime
 
-from typing import List
-
 from src.press import THEAT
-from src.orders_reader import OrdersReader
+from src.orders_io import get_orders, save_orders
+
 from src.press_working_strategy import DoShortOrderAndStopStrategy
 from src.press_working_strategy import CheckNextOrderBeforeStopStrategy
 from src.press_working_strategy import DoLongestOrderStrategy
 
 
-from src.order import Order
 from src.optimize import brute_force_optimize
+
+DATAFILE = "../data/orders.xlsx"
+OUTDIR = "../result/"
 
 STRATEGY_DO_SHORT_ORDER = "do-short-order"
 STRATEGY_DO_LONG_ORDER = "do-long-order"
 STRATEGY_CHK_NEXT_ORDER = "chk-next-order"
-
-
-def get_orders(num_orders: int) -> List[Order]:
-    filename = "../data/orders.xlsx"
-    orders = OrdersReader(filename, num_orders)
-    order_names = orders.get_matricies_names()
-    return [
-        Order(ix, dur, order_names[ix])
-        for ix, dur in orders.get_orders_duration().items()
-    ]
-
-
-def save_orders(
-    outfile: str,
-    strategy: str,
-    best_time: int,
-    best_cycle_number: int,
-    best_sequence: List[List[Order]],
-) -> None:
-    with open(outfile, "w") as out:
-        print(f"Strategy = {strategy}", file=out)
-        print(f"BestTime = {best_time}", file=out)
-        print(f"Cycles = {best_cycle_number}", file=out)
-        for i, seq in enumerate(best_sequence):
-            order_indexes = ", ".join(str(order.get_order_index()) for order in seq)
-            print(f"{i}: {order_indexes}", file=out)
 
 
 @click.command()
@@ -56,13 +31,9 @@ def save_orders(
     help="Strategy to deal with current orders under process.",
 )
 @click.option(
-    "--batchsize",
-    default=20000,
-    type=int,
-    help="Batch size for paralell computation.",
+    "--batchsize", default=20000, type=int, help="Batch size for paralell computation.",
 )
 def main(num_orders, strategy, batchsize):
-    outfile = "../result/"
     time_start = datetime.datetime.now()
     print(f"Time start = {time_start.strftime('%Y-%m-%d %H:%M:%S')}")
     orders = get_orders(num_orders)
@@ -79,7 +50,7 @@ def main(num_orders, strategy, batchsize):
         orders, work_strategy, batchsize
     )
 
-    outfile += f"brute-{strategy}-{num_orders}-orders.txt"
+    outfile = f"{OUTDIR}brute-{strategy}-{num_orders}-orders.txt"
     save_orders(outfile, strategy, best_time, best_cycle_number, best_sequence)
 
     print(f"best time = {best_time}")
